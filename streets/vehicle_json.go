69142a7ec3482e8d7cca68bf1567c71f258bdb24
Valerius Mattfeld <mail@valerius.me>
2023-08-15 16:21:43 +0200

    fix: vehilce serialization

--- streets/vehicle_json.go
+++ streets/vehicle_json.go
@@ -3,13 +3,44 @@ package streets
 import "encoding/json"
 
 func UnmarshalVehicle(data []byte) (Vehicle, error) {
-	var r Vehicle
+	var r rawVehicle
 	err := json.Unmarshal(data, &r)
-	return r, err
+	return Vehicle{
+		ID:                r.ID,
+		PathIDs:           r.PathIDs,
+		Speed:             r.Speed,
+		Delta:             r.Delta,
+		NextID:            r.NextID,
+		PrevID:            r.PrevID,
+		IsParked:          r.IsParked,
+		DistanceRemaining: r.DistanceRemaining,
+		StreetGraph:       nil,
+		MarkedForDeletion: false,
+	}, err
 }
 
 func (v *Vehicle) Marshal() ([]byte, error) {
-	return json.Marshal(v)
+	return json.Marshal(rawVehicle{
+		ID:                v.ID,
+		PathIDs:           v.PathIDs,
+		Speed:             v.Speed,
+		Delta:             v.Delta,
+		NextID:            v.NextID,
+		PrevID:            v.PrevID,
+		IsParked:          v.IsParked,
+		DistanceRemaining: v.DistanceRemaining,
+	})
+}
+
+type rawVehicle struct {
+	ID                string  `json:"id"`
+	PathIDs           []int   `json:"path_ids"`
+	Speed             float64 `json:"speed"`
+	Delta             float64 `json:"delta"`
+	NextID            int     `json:"next_id"`
+	PrevID            int     `json:"prev_id"`
+	IsParked          bool    `json:"is_parked"`
+	DistanceRemaining float64 `json:"distance_remaining"`
 }
 
 type Vehicle struct {
